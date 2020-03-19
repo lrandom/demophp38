1ae3d3be81d546dfa82b943340582511c1e8dce3
root <luann4099@gmail.com>
2020-03-19 19:30:22 +0700

    thêm bài tập

---
+++ session3/index3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php var_dump($_SERVER); ?>
+    <?php var_dump($_POST); ?>
+    <?php var_dump($_GET); ?>
+</body>
+
+</html>
